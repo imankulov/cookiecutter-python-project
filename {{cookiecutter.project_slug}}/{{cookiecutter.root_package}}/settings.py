fedf36367b1790d99e8bc695fb803eea9e366531
Roman Imankulov <roman.imankulov@gmail.com>
2021-03-25 11:16:32 +0000

    Add Django-specific settings
    - Dependencies that include Django, psycopg2, Sentry SDK and
      pytest-django
    - Sample environment file
    - Settings that read contents from the environment

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.root_package}}/settings.py
@@ -0,0 +1,102 @@
+from pathlib import Path
+
+import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+env = environ.Env(
+    DEBUG=bool,
+    ALLOWED_HOSTS=list,
+    WEATHER_CLIENT_ARGS=dict,
+    CORS_ALLOWED_ORIGINS=list,
+    CORS_ALLOW_ALL_ORIGINS=bool,
+)
+
+environ.Env.read_env((BASE_DIR / ".env").as_posix())
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "{{ cookiecutter.root_package }}.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "{{ cookiecutter.root_package }}.wsgi.application"
+
+
+# Database
+DATABASES = {
+    "default": env.db(),
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+
+# Sentry settings
+sentry_sdk.init(
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
