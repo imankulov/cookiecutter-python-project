838db1d474eacae3e9474676f5d2edfe6f9e3d83
Roman Imankulov <roman.imankulov@gmail.com>
2021-04-25 20:31:48 +0100

    Add support for project templates

--- {{cookiecutter.project_slug}}/{{cookiecutter.root_package}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.root_package}}/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = "{{ cookiecutter.root_package }}.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "{{ cookiecutter.root_package }}" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
